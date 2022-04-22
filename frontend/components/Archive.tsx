948423bddfac389f7c3d70cefbcb2664a7a1e3ef
margacampos <margacampos@hotmail.es>
2022-04-22 15:32:39 +0100

    styling

--- frontend/components/Archive.tsx
+++ frontend/components/Archive.tsx
@@ -8,9 +8,9 @@ export default function Archive (props: IArchiveProps) {
     const [objPosition, setObjPosition] = useState("center")
   return (
     <div>
-      <Image src={"/archive/archive.png"} alt="GDELT archive background" width={1200} height={400} layout="fill" objectFit='cover' objectPosition="center"/>
+      <Image src={"/archive/archive.png"} alt="GDELT archive background" layout="fill" objectFit='cover' objectPosition="center"/>
       <button onClick={()=>{setObjPosition((state)=>state==="center"?"0px 100px":"center")}}>
-        <Image src={"/archive/archive-gabinet.png"} alt="GDELT archive gabinet" width={1200} height={400} layout="fill" objectFit='cover' objectPosition={objPosition}/> 
+        <Image src={"/archive/archive-gabinet.png"} alt="GDELT archive gabinet" layout="fill" objectFit='cover' objectPosition={objPosition}/> 
       </button>
     </div>
   );
