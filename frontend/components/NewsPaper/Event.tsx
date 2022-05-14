6fee72b4395fe0aa41a350d685b7bfc5aae13d51
margacampos <margacampos@hotmail.es>
2022-05-14 20:28:46 +0100

    Event.module.scss

--- frontend/components/NewsPaper/Event.tsx
+++ frontend/components/NewsPaper/Event.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { EventByLocAct, genRandomEvent, getActorCodeLabel, getEventCodeLabel, getEventName, getLocCodeLabel, organizeByLocAndAct } from '../../utils/eventUtils';
-
+import styles from "../../styles/events/Event.module.scss"
 type Props = {
     event:EventByLocAct;
 }
@@ -16,8 +16,10 @@ export default function Event({event}: Props) {
   }, [])
   
   return (
-    <div>
+    <div className={styles.event}>
+      <hr />
         <h2>Event: {ev && ev.name}</h2>
+        <hr />
         <div>
             <h3>In this event...</h3>
             {ev && ev.participants[0] && <p>Actors: {ev.participants?.map((i:{value:string},index:number)=>index==0?getActorCodeLabel(i.value):` and ${getLocCodeLabel(i.value)}`)}</p>}
--- frontend/pages/display.tsx
+++ frontend/pages/display.tsx
@@ -6,13 +6,11 @@ import { genRandomEvent } from "../utils/eventUtils";
 
 const Display: NextPage = () => {
     const Router = useRouter();
-    const [event, setEvent] = useState(genRandomEvent(1))
-
-    
+    const [event, setEvent] = useState(genRandomEvent(3))
     
     return (
-      <div>
-        {event && <Event event={event[0]}/>}
+      <div className={styles.display}>
+        {event && event.map((i,index)=><Event key={index} event={i}/>) }
       </div>
     )
   }
