0257edbc2f77a197b2fb6ab3ce277ac21649a41f
margacampos <margacampos@hotmail.es>
2022-05-14 20:36:51 +0100

    SelectEvent.tsx

---
+++ frontend/components/Game/SelectEvent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "../styles/events/Display.module.scss"
+
+type Props = {}
+
+export default function SelectEvent({}: Props) {
+    
+  return (
+    <div className={styles.display}>
+        
+    </div>
+  )
+}
