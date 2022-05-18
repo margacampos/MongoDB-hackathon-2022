bfd9510e17bd78b16642b2de6e3f64aa76473a64
margacampos <margacampos@hotmail.es>
2022-05-18 15:04:43 +0100

    SelectEvents interaction

--- frontend/components/Game/SelectEvent.tsx
+++ frontend/components/Game/SelectEvent.tsx
@@ -4,13 +4,15 @@ import { useEffect, useState } from "react";
 import Event from "../../components/NewsPaper/Event"
 import { genRandomEvent } from "../../utils/eventUtils";
 
-type Props = {}
+type Props = {
+  getNextInteraction:(punctuation?:number)=>void;
+}
 
-export default function SelectEvent({}: Props) {
+export default function SelectEvent({getNextInteraction}: Props) {
     const [event, setEvent] = useState(genRandomEvent(3))
   return (
     <div className={styles.display}>
-        {event && event.map((i,index)=><Event key={index} event={i}/>) }
+        {event && event.map((i,index)=><button key={index} onClick={()=>getNextInteraction()}><Event event={i}/></button>) }
     </div>
   )
 }
