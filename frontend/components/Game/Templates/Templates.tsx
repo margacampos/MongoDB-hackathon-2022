e6867225a21c4d636318561b41383c2072f096e0
margacampos <margacampos@hotmail.es>
2022-05-19 19:13:30 +0100

    Templates

---
+++ frontend/components/Game/Templates/Templates.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react'
+import styles from "../../../styles/templates/Templates.module.scss"
+import Template1 from './Template1';
+import Template2 from './Template2';
+import Template3 from './Template3';
+type Props = {
+    title:string;
+}
+
+export default function Templates({title}: Props) {
+    const [displayTitle, setDisplayTitle] = useState([""]);
+    const arrangeTitle = (title:string) =>{
+        let words = title.split(" ");
+        let rows = []
+        let finish = false;
+        let last = 0;         
+        while (finish==false) {
+            let i=0;
+            let quote = ""
+            if(words.length===0){setDisplayTitle(rows); finish=true;};
+            for(i; i<words.length;i++){
+            if(words[i].length+quote.length<27){
+                    quote+=`${words[i]} `
+                    words.splice
+                }else{
+                    rows.push(quote);
+                    last = i;
+                    break;
+                }
+              }
+              words.splice(0,i);
+              if(words.length===0){rows.push(quote); setDisplayTitle(rows); finish=true;};
+        }
+    }
+    useEffect(() => {
+      if(title.length>27){
+         arrangeTitle(title);
+      }else{
+          setDisplayTitle([title])
+      }
+      return () => {
+        
+      }
+    }, []);
+    
+  return (
+      <div className={styles.templates}>
+        <div className={styles.newspaper}>
+            <button className={styles.img}>
+                <Template1 title={displayTitle} date="November 20th, 2022" website='hola.es'/>
+            </button>
+            
+        </div>  
+        <div className={styles.newspaper}>
+            <button className={styles.img}>
+                <Template2 title={displayTitle} date={"November 20th, 2022"} website="hola.es"/>
+            </button>
+        </div> 
+        <div className={styles.newspaper}>
+            <button className={styles.img}>
+                <Template3 title={displayTitle} date={"November 20th, 2022"} website="hola.es"/>
+            </button>
+        </div> 
+      </div>
+    
+  )
+}
--- frontend/pages/display.tsx
+++ frontend/pages/display.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import GameScreen from "../components/Game/GameScreen";
+import Templates from "../components/Game/Templates/Templates";
 
 const Display: NextPage = () => {
     const Router = useRouter();
@@ -8,6 +9,7 @@ const Display: NextPage = () => {
     return (
       <div>
         {/* <GameScreen gameObject={game} setGame={setGame}/> */}
+        <Templates title="Este es el mejor título ever but it is just tooo long what can I do oh goodness gracious"/>
       </div>
     )
   }
