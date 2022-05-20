94487ca13d5f298eba0826219af52bc47f059f81
margacampos <margacampos@hotmail.es>
2022-05-20 19:54:12 +0100

    clueDialogs

--- frontend/data/dialogsNew.ts
+++ frontend/data/dialogsNew.ts
@@ -57,12 +57,12 @@ const clueDialogs = [
             actor:{
                 knows:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
                 knowsNot:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
-                dontknow:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]]
+                dontknow:[["Mmm... I'm not sure.", "I think you should ask someone else, maybe Thomas knows."],["I have so much in my head right now but let me think... what was it...", "I'm sorry I can't recall."],["I don't know but I'm sure someone else does.", "If you ask around I'm sure someone will have the answer."]]
             },
             eventCode:{
                 knows:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
                 knowsNot:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
-                dontknow:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]]
+                dontknow:[["Mmm... I'm not sure.", "I think you should ask someone else, maybe Aaliyah knows."],["I have so much in my head right now but let me think... what was it...", "I'm sorry I can't recall."],["I don't know but I'm sure someone else does.", "If you ask around I'm sure someone will have the answer."]]
             },
         }
     },
@@ -72,17 +72,17 @@ const clueDialogs = [
             location:{
                 knows:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
                 knowsNot:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
-                dontknow:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]]
+                dontknow:[["Mmm... I'm not sure.", "I think you should ask someone else, maybe Thomas knows."],["I have so much in my head right now but let me think... what was it...", "I'm sorry I can't recall."],["I don't know but I'm sure someone else does.", "If you ask around I'm sure someone will have the answer."]]
             },
             actor:{
                 knows:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
                 knowsNot:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
-                dontknow:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]]
+                dontknow:[["Mmm... I'm not sure.", "I think you should ask someone else, maybe Gabrielle knows."],["I have so much in my head right now but let me think... what was it...", "I'm sorry I can't recall."],["I don't know but I'm sure someone else does.", "If you ask around I'm sure someone will have the answer."]]
             },
             eventCode:{
                 knows:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
                 knowsNot:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
-                dontknow:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]]
+                dontknow:[["Mmm... I'm not sure.", "I think you should ask someone else, maybe Aaliyah knows."],["I have so much in my head right now but let me think... what was it...", "I'm sorry I can't recall."],["I don't know but I'm sure someone else does.", "If you ask around I'm sure someone will have the answer."]]
             },
         }
     },
@@ -92,17 +92,17 @@ const clueDialogs = [
             location:{
                 knows:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
                 knowsNot:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
-                dontknow:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]]
+                dontknow:[["Mmm... I'm not sure.", "I think you should ask someone else, maybe Gabrielle knows."],["I have so much in my head right now but let me think... what was it...", "I'm sorry I can't recall."],["I don't know but I'm sure someone else does.", "If you ask around I'm sure someone will have the answer."]]
             },
             actor:{
                 knows:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
                 knowsNot:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
-                dontknow:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]]
+                dontknow:[["Mmm... I'm not sure.", "I think you should ask someone else, maybe Aaliyah knows."],["I have so much in my head right now but let me think... what was it...", "I'm sorry I can't recall."],["I don't know but I'm sure someone else does.", "If you ask around I'm sure someone will have the answer."]]
             },
             eventCode:{
                 knows:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
                 knowsNot:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
-                dontknow:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]]
+                dontknow:[["Mmm... I'm not sure.", "I think you should ask someone else, maybe Jakub knows."],["I have so much in my head right now but let me think... what was it...", "I'm sorry I can't recall."],["I don't know but I'm sure someone else does.", "If you ask around I'm sure someone will have the answer."]]
             },
         }
     },
@@ -112,17 +112,17 @@ const clueDialogs = [
             location:{
                 knows:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
                 knowsNot:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
-                dontknow:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]]
+                dontknow:[["Mmm... I'm not sure.", "I think you should ask someone else, maybe Thomas knows."],["I have so much in my head right now but let me think... what was it...", "I'm sorry I can't recall."],["I don't know but I'm sure someone else does.", "If you ask around I'm sure someone will have the answer."]]
             },
             actor:{
                 knows:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
                 knowsNot:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
-                dontknow:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]]
+                dontknow:[["Mmm... I'm not sure.", "I think you should ask someone else, maybe Gabrielle knows."],["I have so much in my head right now but let me think... what was it...", "I'm sorry I can't recall."],["I don't know but I'm sure someone else does.", "If you ask around I'm sure someone will have the answer."]]
             },
             eventCode:{
                 knows:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
                 knowsNot:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]],
-                dontknow:[["This week's event should be focused on $.", "Anything else you need?"],["I heard from Thomas that there was a great story to focus on in $.", "Hope I was of help."],["Well... I'm not sure...", "If I can recall correctly there was a great event in $ but I would ask someone else if I were you."]]
+                dontknow:[["Mmm... I'm not sure.", "I think you should ask someone else, maybe Jakub knows."],["I have so much in my head right now but let me think... what was it...", "I'm sorry I can't recall."],["I don't know but I'm sure someone else does.", "If you ask around I'm sure someone will have the answer."]]
             },
         }
     }
